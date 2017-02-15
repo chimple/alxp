9ee111d922f8266f604e3d4ea3101a81aa619fba
shyamalu <Shyamal.Upadhyaya@Shyamals-MacBook-Pro.local>
2017-02-15 11:51:22 +0530

    TTS changes

---
+++ app/src/main/java/org/awesomeapp/messenger/tts/TextToSpeechEventListener.java
@@ -0,0 +1,22 @@
+package org.awesomeapp.messenger.tts;
+
+import android.speech.tts.TextToSpeech;
+
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Created by Shyamal.Upadhyaya on 14/02/17.
+ */
+
+public interface TextToSpeechEventListener {
+
+    public void onSuccessfulInitiated(TextToSpeech tts);
+
+    public void onDownloadRequiredForLanguageData(List<Locale> missingLocals);
+
+    public void onDownloadNotCompletedForLanguageData();
+
+    public void onErrorToInitialize();
+
+}
